ac8a0077bbee8a4e704b1bfc68045a3d26ae6f94
Beth Carretta <bethcarretta@gmail.com>
2023-08-11 15:07:05 -0400

    longest passing tests

--- recursion.js
+++ recursion.js
@@ -9,9 +9,28 @@ function product(nums) {
   return nums[0] * product(nums.slice(1))
 }
 
-/** longest: return the length of the longest word in an array of words. */
+/** longest: return the length of the longest word in an array of words.
+ * longest(["hello", "hi", "hola"]) // 5
+ * longest(["abcdefg", "hijklmnop", "qrs", "tuv", "wx", "y", "z"]) // 9
+ * longest(["a", "b", "c", "d", "e"]) // 1
+ * longest(["abcde"]) // 5
+ */
 
-function longest(words) {}
+function longest(words, i = 0) {
+  // base case --> ["a
+  if (words.length === 1) return words[0].length
+  // recursive case
+  // compare length of first word with length of longest word in array
+  const firstWordLength = words[0].length
+  const longestRest = longest(words.slice(1), i + 1)
+
+  // if first word is longer, then it is the new longest word
+  if (firstWordLength > longestRest) {
+    return firstWordLength
+  } else {
+    return longestRest
+  }
+}
 
 /** everyOther: return a string with every other letter. */
 
