52cb73e08dc6332f4e52ea55d503bb2571547c8f
Beth Carretta <bethcarretta@gmail.com>
2023-08-12 05:49:13 -0400

    isPalindrome passing

--- recursion.js
+++ recursion.js
@@ -41,9 +41,23 @@ function everyOther(str, i = 0) {
   }
 }
 
-/** isPalindrome: checks whether a string is a palindrome or not. */
+/** isPalindrome: checks whether a string is a palindrome or not.
+ *
+ *isPalindrom(tacocat) // true
+ isPalindrome("a") // true
+ isPalindrome("goodbye") // false
+ t = t str[0] === str[str.length -1]
+ str[i +1] === str[str.length - 2]
+ str[i + 2] === str[str.length -3]
 
-function isPalindrome(str) {}
+ */
+
+function isPalindrome(str, first = 0, last = str.length - 1) {
+  if (first >= last) return true
+  if (str[first] !== str[last]) return false
+
+  return isPalindrome(str, first + 1, last - 1)
+}
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
